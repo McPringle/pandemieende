d71f8b9cc54fb98d1fdd7e96f32f3506cad55dae
Marcus Fihlon <marcus@fihlon.swiss>
2021-02-24 21:25:40 +0100

    :sparkles: Adding Covid-19 cases and deaths

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ if (location.protocol !== 'https:' && !location.href.includes("://localhost"))
     location.href = 'https:' + location.href.substring(location.protocol.length);
 }
 
+const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+const residents = 8606033 + 38650; // Schweiz und Liechtenstein (Stand 2019, Bundesamt für Statistik)
+
 function toDate(dateString) {
     return new Date(dateString + 'T00:00:00.000Z');
 }
@@ -38,9 +41,14 @@ function toDurationString(numberOfDays) {
     return years + " " + yearText + ", " + months + " " + monthText + ", " + days + " " + dayText;
 }
 
+function toPercent(vaccinatedPersons) {
+    return Math.round(vaccinatedPersons * 100 / residents * 100) / 100;
+}
+
 function updateChart(vaccinationDataHistory) {
     const vaccinationStatusDates = vaccinationDataHistory.map(function (row) { return row['statusDate']; });
     const vaccinationDataPercent = vaccinationDataHistory.map(function (row) { return row['vaccinationRate']; });
+    const vaccinationDataPercentImmune = vaccinationDataHistory.map(function (row) { return toPercent(row['vaccinatedPersons'] + row['laboratoryConfirmedCases'] - row['laboratoryConfirmedDeaths']); });
     const threshold = new Array(vaccinationDataPercent.length).fill(70);
     const ctx = document.getElementById('vaccinationChart').getContext('2d');
     new Chart(ctx, {
@@ -48,8 +56,9 @@ function updateChart(vaccinationDataHistory) {
         data: {
             labels: vaccinationStatusDates,
             datasets: [
-                { label: 'vollständig geimpft', data: vaccinationDataPercent, borderColor: '#dc1c13', borderWidth: 3, fill: false },
-                { label: 'Herdenimmunität (70%)', data: threshold, borderColor: '#69f0ae', borderWidth: 1, fill: false }
+                { label: 'vollständig geimpfte Personen', data: vaccinationDataPercent, borderColor: '#dc1c13', borderWidth: 3, fill: false },
+                { label: 'vollständig geimpfte & gesundete Personen', data: vaccinationDataPercentImmune, borderColor: '#f9842c', borderWidth: 3, fill: false },
+                { label: 'nötige Herdenimmunität (70%)', data: threshold, borderColor: '#69f0ae', borderWidth: 1, fill: false }
             ]
         },
         options: {
@@ -73,12 +82,10 @@ function updateChart(vaccinationDataHistory) {
 }
 
 function updateTable(vaccinationDataHistory, lastUpdate) {
-    const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
 
     const statusDate = toDate(vaccinationDataHistory[vaccinationDataHistory.length - 1]['statusDate']);
     const administeredVaccineDoses = vaccinationDataHistory[vaccinationDataHistory.length - 1]['administeredVaccineDoses'];
 
-    const residents = 8606033 + 38650; // Schweiz und Liechtenstein (Stand 2019, Bundesamt für Statistik)
     const toBeVaccinated = Math.round(residents * .7);
     const vaccinatedPersons = vaccinationDataHistory[vaccinationDataHistory.length - 1]['vaccinatedPersons'];
     const stillToBeVaccinated = toBeVaccinated - vaccinatedPersons;
@@ -101,6 +108,9 @@ function updateTable(vaccinationDataHistory, lastUpdate) {
     document.getElementById("toBeVaccinated").innerHTML = toNumberString(toBeVaccinated);
     document.getElementById("vaccinatedPersons").innerHTML = toNumberString(vaccinatedPersons);
     document.getElementById("vaccinationRateLast").innerHTML = toNumberString(vaccinationRateLast);
+
+    document.getElementById("laboratoryConfirmedCases").innerHTML = toNumberString(vaccinationDataHistory[vaccinationDataHistory.length - 1]['laboratoryConfirmedCases']);
+    document.getElementById("laboratoryConfirmedDeaths").innerHTML = toNumberString(vaccinationDataHistory[vaccinationDataHistory.length - 1]['laboratoryConfirmedDeaths']);
 
     const vaccinationRateDaysElements = document.getElementsByClassName("vaccinationRateDays");
     for (let i = 0; i < vaccinationRateDaysElements.length; i++) {
